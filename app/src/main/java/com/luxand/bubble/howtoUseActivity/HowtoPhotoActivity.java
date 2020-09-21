928801b657cbd442c13f941af7f85477e76bd8cf
Kimhagyeong <abcd358583@gmail.com>
2020-09-21 16:07:12 +0900

    howtoactivity X버튼문제해결

--- app/src/main/java/com/luxand/bubble/howtoUseActivity/HowtoPhotoActivity.java
+++ app/src/main/java/com/luxand/bubble/howtoUseActivity/HowtoPhotoActivity.java
@@ -21,11 +21,7 @@ public class HowtoPhotoActivity extends HowtoUseActivity {
         a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(
-                        getApplicationContext(), // 현재 화면의 제어권자
-                        HowtoUseActivity.class); // 다음 넘어갈 클래스 지정
-                intent.addFlags (Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(intent); // 다음 화면으로 넘어간다
+                finish();
             }
         });
     } // end onCreate()
